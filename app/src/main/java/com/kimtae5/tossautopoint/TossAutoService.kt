05486d7b95572adb0b8791281f5822eddacb1278
kimtaeo <104893952+kimtae5@users.noreply.github.com>
2026-08-03 20:15:55 +0900

    TossAutoService.kt 업데이트

--- app/src/main/java/com/kimtae5/tossautopoint/TossAutoService.kt
+++ app/src/main/java/com/kimtae5/tossautopoint/TossAutoService.kt
@@ -309,7 +309,7 @@ class TossAutoService : AccessibilityService() {
                             
                             if (rootNode != null) {
                                 // 오직 틱톡 라이트 전용 ID만 스캔합니다.
-                                val liteNodes = rootNode.findAccessibilityNodeInfosByViewId("com.ss.android.ugc.tiktok.lite:id/ynj")
+                                val liteNodes = rootNode.findAccessibilityNodeInfosByViewId("com.ss.android.ugc.tiktok.lite:id/yq5")
                                 if (liteNodes.isNotEmpty()) {
                                     hasPointIcon = liteNodes.any { it.isVisibleToUser }
                                     liteNodes.forEach { it.recycle() }
